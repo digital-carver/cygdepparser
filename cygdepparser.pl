08c912fff24d2a5265a29b58e30860c892c1556b
SundaraRaman R <sundar@abiteasier.in>
2015-08-20 02:46:10 +0530

    untested additions to use GraphViz.pm

--- cygdepparser.pl
+++ cygdepparser.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use English;
 
+use GraphViz;
+
 my $infilename;
 
 if (@ARGV) {
@@ -21,6 +23,8 @@ open my $infile, '<', $infilename;
 local $INPUT_RECORD_SEPARATOR;
 my $content = <$infile>;
 
+my $gr = GraphViz->new();
+
 # Split the contents (delimited by double \n) into individual pkg dependencies
 my @pkg_deps = split(/\n\s*\n/, $content);
 for my $pkg_dep (@pkg_deps) {
@@ -28,10 +32,19 @@ for my $pkg_dep (@pkg_deps) {
     @lines == 3 or die 'Expected only 3 lines per pkg dependency info, got '
                         . scalar(@lines) .  ". Something's wonky\n";
 
-    my ($pkg_name, $pkg_desc, $pkg_requires) = @lines;
-    $pkg_requires =~ s/^\s+Required by: //;
-    my @requires = split(/, /, $pkg_requires);
+    my ($pkg_name, $pkg_desc, $pkg_dependants) = @lines;
+    
+    #remove the version since 'required by' doesn't list it, so we can't use it
+    $pkg_name =~ s/\t\(.*\)$//; 
 
-    print "$pkg_name requires @requires\n";
+    $pkg_dependants =~ s/^\s+Required by: //;
+    my @dependants = split(/, /, $pkg_dependants);
+
+    print "$pkg_name is required by @dependants\n";
+    #GraphViz ignores this if node already exists, so no need for check
+    $gr->add_node($pkg_name); 
+    $gr->add_edge($_) foreach (@dependants);
 }
+
+$gr->as_svg('cyg_dependency.svg');
 
